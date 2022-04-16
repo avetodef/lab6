8b5bfcddef0095dd8548eca438361dad006c199e
fedoteva <sofiafedoteva@gmail.com>
2022-04-16 12:31:16 +0300

    починили ситуацию когда байт буфер выводит по 9 символов

--- src/main/java/client/ClientApp.java
+++ src/main/java/client/ClientApp.java
@@ -29,7 +29,7 @@ public class ClientApp {
      ConsoleReader consoleReader = new ConsoleReader();
      ConsoleOutputer outputer = new ConsoleOutputer();
      Scanner fromKeyboard = new Scanner(System.in);
-    ByteBuffer buffer = ByteBuffer.allocate(2048);
+    ByteBuffer buffer = ByteBuffer.allocate(40000);
     //TODO обработать пицот миллионов поломок джокера по типу поменял порт, подключил два клиента, еще что-нибудь
     //TODO сделать чтобы передавались не строки а объекты класса реквест и респонз (сообщение егошина момент)
     int serverPort = 6666;
@@ -40,13 +40,13 @@ public class ClientApp {
         String serverResponse;
 
 
-    try {//todo как заставить байт буфер и потоки дружить....
+    try {
 
         SocketChannel socketChannel = SocketChannel.open();
-        socketChannel.configureBlocking(true); //подключается если блокирующий канал... если неблокирующий то не подключается
+        //TODO оно не работает если поставить false...
+        socketChannel.configureBlocking(false); //подключается если блокирующий канал... если неблокирующий то не подключается
 
         //todo нужнл все переделывать селекторами.
-        //socketChannel.bind(new InetSocketAddress("localhost", serverPort));
 
         socketChannel.connect(new InetSocketAddress("localhost", serverPort));
 
@@ -61,45 +61,20 @@ public class ClientApp {
                 ifExit(input, dao);
 
                 if (CommandSaver.checkCommand(toCheck)) {
-
-                    buffer = StandardCharsets.UTF_8.encode(JsonConverter.serialize(input));
-                    //System.out.println(buffer);
-                    //System.out.println(socketChannel.write(buffer));
-                    socketChannel.write(buffer);
-                    System.out.println("capacity:" +  buffer.capacity());
-                    System.out.println("limit: " + buffer.limit());
+                    socketChannel.write(StandardCharsets.UTF_8.encode(JsonConverter.serialize(input)));
 
                     System.out.println("команда отправляется на сервер...");
                 }
                 else
                     throw new NullPointerException("Введённой вами команды не существует. Попробуйте ввести другую команду.");
 
-                //todo передает все по 9 символов... а иногда не по 9 а по рандому. может выкинуть 27 симолов а может 3 символа
-                //todo мне кажется что он выводит только символов какой у него лимит... а как сделать лимит больше я не знаю
-                buffer.clear();
-
-                //TimeUnit.MINUTES.sleep(1);
 
                 socketChannel.read(buffer);
                 buffer.flip();
-                //buffer.limit(100);
-//TODO рассказываю будующей себе. короче он считает за лимит и капасити то сколько букв в твоей команде. и еще он если не успевает вывести
-                // предыдущий резльтат выполнения команды то он продолжает его выводить
-// tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
 
-                System.out.println("capacity:" +  buffer.capacity());
-                System.out.println("limit: " + buffer.limit());
-                System.out.println(buffer.toString());
                 serverResponse = StandardCharsets.UTF_8.decode(buffer).toString();
-                outputer.printWhite("сообщение от сервера: "+serverResponse);
-
-
-//                buffer.clear(); //ничего не записывается в буфер
-//                int bRead = socketChannel.read(buffer);
-//                System.out.println(bRead);
-//                serverResponse = StandardCharsets.UTF_8.decode(buffer).toString(); //ну конечно же)
-
-                //System.out.println(serverResponse); //печатает что сервер респонз пустой...
+                System.out.println(serverResponse);
+                buffer.clear();
 
 
             }
